e01bd1de84a4b0eb84259a1afd1feec4b7e37115
fnikitin <frederic.nikitin@gmail.com>
2016-07-15 20:17:30 +0200

    Client to call RawStatement REST api on kant

--- src/main/java/org/nextprot/dao/statements/RawStatementRemoteService.java
+++ src/main/java/org/nextprot/dao/statements/RawStatementRemoteService.java
@@ -1,22 +1,36 @@
 package org.nextprot.dao.statements;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.nextprot.commons.statements.RawStatement;
+import org.springframework.stereotype.Service;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 
-import org.nextprot.commons.statements.RawStatement;
-import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @Service
 public class RawStatementRemoteService {
 
-	private String serviceUrl = "http://kant.isb-sib.ch:9000/gene/";
+	private String serviceUrl = "http://kant.isb-sib.ch:9000";
 
-	public List<RawStatement> getGene(String geneName) {
-		String url = serviceUrl + geneName + "/statements";
+	public RawStatementRemoteService(String datasource) {
+		serviceUrl = "http://kant.isb-sib.ch:9000/"+datasource;
+	}
+
+	// BioEditor Raw Statement service for a Gene. Example for msh2: http://kant.isb-sib.ch:9000/bioeditor/gene/msh2/statements
+	public List<RawStatement> getGeneRawStatementList(String geneName) {
+
+		return deserialize(serviceUrl + "/gene/"+ geneName + "/statements");
+	}
+
+	// BioEditor Raw Statement service for all data (CAREFUL WITH THIS ONE) http://kant.isb-sib.ch:9000/bioeditor/statements
+	public List<RawStatement> getAllGenesRawStatementList() {
+
+		return deserialize(serviceUrl + "/statements");
+	}
+
+	private List<RawStatement> deserialize(String url) {
 
 		ObjectMapper mapper = new ObjectMapper();
 
@@ -30,7 +44,5 @@ public class RawStatementRemoteService {
 		}
 
 		return obj;
-
 	}
-
 }
